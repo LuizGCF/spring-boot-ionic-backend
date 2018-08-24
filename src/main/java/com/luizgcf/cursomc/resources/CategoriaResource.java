82b32bb2624050552785abff6404c834afdbb6ae
Luiz Gonzaga <lgcf97@gmail.com>
2018-08-24 11:23:01 -0300

    Deleting a 'Categoria' with DELETE

--- src/main/java/com/luizgcf/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/luizgcf/cursomc/resources/CategoriaResource.java
@@ -41,4 +41,10 @@ public class CategoriaResource {
 		System.out.println("A "+obj.getId());
 		return ResponseEntity.noContent().build();
 	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
 }
--- src/main/java/com/luizgcf/cursomc/services/CategoriaService.java
+++ src/main/java/com/luizgcf/cursomc/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.luizgcf.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.luizgcf.cursomc.domain.Categoria;
 import com.luizgcf.cursomc.repositories.CategoriaRepository;
+import com.luizgcf.cursomc.services.exceptions.DataIntegrityException;
 import com.luizgcf.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -30,5 +32,14 @@ public class CategoriaService {
 		System.out.println("B "+obj.getId());
 		find(obj.getId());
 		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos.");
+		}
 	}
 }
