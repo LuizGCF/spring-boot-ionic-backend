c0ddd5c97fb2e474bb0fa6ce50797edb1aa97da3
Luiz Gonzaga <lgcf97@gmail.com>
2018-09-04 10:28:45 -0300

    Content restriction: 'Cliente' can only retrieve his own orders

--- src/main/java/com/luizgcf/cursomc/services/PedidoService.java
+++ src/main/java/com/luizgcf/cursomc/services/PedidoService.java
@@ -6,8 +6,12 @@ import java.util.Optional;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.luizgcf.cursomc.domain.Cliente;
 import com.luizgcf.cursomc.domain.ItemPedido;
 import com.luizgcf.cursomc.domain.PagamentoComBoleto;
 import com.luizgcf.cursomc.domain.Pedido;
@@ -15,6 +19,8 @@ import com.luizgcf.cursomc.domain.enums.EstadoPagamento;
 import com.luizgcf.cursomc.repositories.ItemPedidoRepository;
 import com.luizgcf.cursomc.repositories.PagamentoRepository;
 import com.luizgcf.cursomc.repositories.PedidoRepository;
+import com.luizgcf.cursomc.security.UserSS;
+import com.luizgcf.cursomc.services.exceptions.AuthorizationException;
 import com.luizgcf.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -68,5 +74,15 @@ public class PedidoService {
 		itemPedidoRepository.saveAll(obj.getItens());
 		emailService.sendOrderConfirmationEmail(obj);
 		return obj;
+	}
+	
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		UserSS user = UserService.authenticated();
+		if (user == null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente =  clienteService.find(user.getId());
+		return repo.findByCliente(cliente, pageRequest);
 	}
 }
